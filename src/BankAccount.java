7ab153fc91df7c05f5b8c2a604530b4409281d6e
DETorz <137002616+DETorz@users.noreply.github.com>
2025-01-29 06:40:28 +0000

    Assignment complete

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
-
+public class BankAccount
+{
+    public static void main(String[] args)
+    {
+        double BOB_BALANCE = 2175.37;
+        BOB_BALANCE -= 302.50;
+        BOB_BALANCE += 50.03;
+        BOB_BALANCE /= 2;
+        BOB_BALANCE += 20;
+        --BOB_BALANCE;
+        BOB_BALANCE *= 2;
+        ++BOB_BALANCE;
+        System.out.println("Bob's final balance is: " + BOB_BALANCE);
+    }
+}
